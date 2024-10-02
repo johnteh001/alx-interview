7e33f82a39c45cd349539d051f531c3a337c2ca2
john karson <johncason43@gmail.com>
2024-10-02 11:12:12 -0700

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 """
-Pascal's Triangle
+0-main
 """
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    triangle = [[1]]
-    
-    for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        for j in range(1, i):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-        new_row.append(1)
-        triangle.append(new_row)
-    
-    return triangle
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
 
 if __name__ == "__main__":
-    def print_triangle(triangle):
-        for row in triangle:
-            print("[{}]".format(",".join([str(x) for x in row])))
-
-    print_triangle(pascal_triangle(5))
+    print_triangle(pascal_triangle(6))
