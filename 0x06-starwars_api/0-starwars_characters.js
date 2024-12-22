a02ad6c6aff2b96aa54c644ea9f0e20f8f02eda7
john karson <casonj254@gmail.com>
2024-12-22 07:21:37 +0000

    update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const arg = process.argv[2];
 
-function makeRequest (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (err, res, body) => {
-      if (!err && res.statusCode === 200) {
-        resolve(JSON.parse(body));
-      } else {
-        reject(err);
-      }
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
-
-const url = 'https://swapi-api.hbtn.io/api/films/'.concat('' + arg);
-async function main () {
-  const film = await makeRequest(url);
-  for (const cha of film.characters) {
-    const character = await makeRequest(cha);
-    console.log(character.name);
-  }
-}
-main();
+};
